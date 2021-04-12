ad365902cb149bd16844e82980f3a216be7a875c
Reid Johnson <rjsa210@gmail.com>
2021-04-12 15:00:09 -0700

    previous repo file structere was flawed, new repo

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+let todoList = [];
+const noteList = JSON.parse(fs.readFileSync('./Develop/db/db.json'));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/develop/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/develop/public/notes.html')));
+app.get('/api/notes', (req, res) => res.json(noteList));
+
+app.post('/api/notes', (req,res) => {
+  // const newNote = req.body;
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4()
+  };
+  todoList = noteList;
+  todoList.push(newNote);
+
+  
+  fs.writeFileSync('../db/db.json', JSON.stringify(todoList, null, 2));
+  res.send(todoList);
+
+});
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
